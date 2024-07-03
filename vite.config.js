36f198a736068c033144394cee0a88c457a73d50
105555 <fter2257@gmail.com>
2024-07-03 17:20:11 +0800

    fix vite.config repo name

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/Week8_AI_Group/',
+  base: '/Design_AIWebsite_Group/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
