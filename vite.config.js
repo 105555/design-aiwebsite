bbf91848e7d3e8071c16b94da0683b99d258398e
105555 <fter2257@gmail.com>
2024-09-25 16:20:41 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/design-aiwebsite_group/',
+  base: '/design-aiwebsite/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
